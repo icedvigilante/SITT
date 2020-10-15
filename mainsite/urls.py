8bf40eeb1812c870395aafd53636f2fdf6c805fc
icedvigilante <wm.ryan.mooney@gmail.com>
2020-10-15 15:13:08 -0800

    Fix js static

--- mainsite/urls.py
+++ mainsite/urls.py
@@ -4,6 +4,6 @@ from .views import IndexView, OriginScriptsView, UserEnvScriptsView
 app_name = 'mainsite'
 urlpatterns = [
     path('', IndexView.as_view(), name='home'),
-    path('origin_scripts', OriginScriptsView.as_view(), name='origin_scripts'),
-    path('user_env_script', UserEnvScriptsView.as_view(), name='user_env_script'),
+    path('origin_scripts/', OriginScriptsView.as_view(), name='origin_scripts'),
+    path('user_env_script/', UserEnvScriptsView.as_view(), name='user_env_script'),
 ]
