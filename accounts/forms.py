884b94a430a72d468783b74ade08d1a16ef21a08
icedvigilante <wm.ryan.mooney@gmail.com>
2020-10-16 12:45:10 -0800

    configured login, logout, signup templates

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,9 +4,19 @@ from .models import CustomUser
 
 
 class CustomUserCreationForm(UserCreationForm):
-    class Meta(UserCreationForm):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+
+    class Meta:
         model = CustomUser
-        fields = ('username', 'email', 'age')
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super(CustomUserCreationForm, self). __init__(*args, **kwargs)
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
 
 
 class CustomUserChangeForm(UserChangeForm):
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404, HttpResponse
 from django.urls import reverse_lazy
+from django.contrib.auth import authenticate, login
 from django.views.generic import CreateView
 from .forms import CustomUserCreationForm
 
 
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy('devarea:home')
     template_name = 'registration/signup.html'
+
+    def form_valid(self, form):
+        form.save()
+        username = self.request.POST['username']
+        password = self.request.POST['password1']
+        user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password1'], )
+        login(self.request, user)
+        return redirect(self.success_url)
+
