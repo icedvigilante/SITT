69a463bc8a7de9e9d38d05c8a7c449035c99374b
icedvigilante <wm.ryan.mooney@gmail.com>
2020-10-16 16:39:49 -0800

    add category for migration

--- blog/models.py
+++ blog/models.py
@@ -6,17 +6,17 @@ from django.contrib.auth.models import Group
 from django.urls import reverse
 
 
-# class Category(models.Model):
-#     name = models.CharField(max_length=255)
-#
-#     class Meta:
-#         ordering = ['name']
-#
-#     def __str__(self):
-#         return self.name
-#
-#     def get_absolute_url(self):
-#         return reverse('blog:home')
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('blog:home')
 
 
 class BlogPost(models.Model):
